3dc2a7f4bea5bfc1099c899eff0aa286efd3de0e
Shawon-Developer <shawonkumarsarkar.developer@gmail.com>
2025-06-09 09:50:31 +0600

    Selected seat info added and total price counted

--- script/main.js
+++ script/main.js
@@ -16,6 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const seatBtnS = document.querySelectorAll(".seat-btn");
   const availableSeatsElement = document.querySelector("#availableSeats");
   let availableSeats = parseInt(availableSeatsElement.textContent);
+  const seatSelected = document.querySelector("#seatSelected");
+  let seatSelectedInt = parseInt(seatSelected.textContent);
+  const selectedSeatInfoContainer = document.querySelector("#selectedSeatInfo");
+  const seatCost = parseInt(document.querySelector("#seatCost").textContent);
+  const totalTicketPrice = document.querySelector("#totalTicketPrice");
 
   seatBtnS.forEach(function (seatBtn) {
     seatBtn.addEventListener("click", function (event) {
@@ -27,6 +32,23 @@ document.addEventListener("DOMContentLoaded", function () {
         // Change Seats Buttons
         event.target.classList.replace("btn-secondary", "btn-primary");
         event.target.setAttribute("disabled", "true");
+
+        // Update Selected Seat Info Container & Selected Seat Count
+        seatSelectedInt++;
+        seatSelected.textContent = seatSelectedInt;
+
+        const newDiv = document.createElement("div");
+        const seatInfoHTML = `
+        <p>${seatBtn.textContent}</p>
+        <p>Business</p>
+        <p>${seatCost}</p>
+        `;
+        newDiv.innerHTML = seatInfoHTML;
+        newDiv.classList.add("seat-info");
+
+        selectedSeatInfoContainer.append(newDiv);
+
+        totalTicketPrice.textContent = seatSelectedInt * seatCost;
       }
 
       if (availableSeats <= 48) {
