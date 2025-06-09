d96916cff17121ea1275ae4ad323199a30766525
Shawon-Developer <shawonkumarsarkar.developer@gmail.com>
2025-06-09 08:58:18 +0600

    Seat secection complete

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Sticky Navigation ---------->
   const navBar = document.querySelector("#SeatSnapNavBar");
-  let currentScrolledNumber = 0;
 
   document.addEventListener("scroll", function (event) {
     if (event.target.defaultView.pageYOffset > 50) {
@@ -10,5 +10,33 @@ document.addEventListener("DOMContentLoaded", function () {
       navBar.classList.remove("sticky-nav");
       currentScrolledNumber = event.target.defaultView.pageYOffset;
     }
+  });
+
+  //Booking Seat Functionality ---------->
+  const seatBtnS = document.querySelectorAll(".seat-btn");
+  const availableSeatsElement = document.querySelector("#availableSeats");
+  let availableSeats = parseInt(availableSeatsElement.textContent);
+
+  seatBtnS.forEach(function (seatBtn) {
+    seatBtn.addEventListener("click", function (event) {
+      if (availableSeats > 48) {
+        // Decrease Seats Count
+        availableSeats--;
+        availableSeatsElement.textContent = availableSeats;
+
+        // Change Seats Buttons
+        event.target.classList.replace("btn-secondary", "btn-primary");
+        event.target.setAttribute("disabled", "true");
+      }
+
+      if (availableSeats <= 48) {
+        seatBtnS.forEach(function (seatBtn) {
+          if (seatBtn.classList.contains("btn-secondary")) {
+            seatBtn.classList.replace("btn-secondary", "btn-error");
+            seatBtn.setAttribute("disabled", "true");
+          }
+        });
+      }
+    });
   });
 });
